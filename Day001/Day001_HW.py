340828f292c92506918459bc05ec2c6f2d24119c
趙宏育 <vito@zhaohongyudeMacBook-Pro.local>
2021-03-18 14:38:38 +0800

    four commit

--- Day001/Day001_HW.py
+++ Day001/Day001_HW.py
@@ -1,17 +1,24 @@
+#作業目標
+#1.（簡答題）檔案、API、爬蟲三種取得資料方式有什麼不同？
+#檔案及API是檔案擁有者主動釋出，爬蟲是被動式釋出
+#2.（實作）完成一個程式，需滿足下列需求：
 from urllib.request import urlretrieve
 import os
-os.makedirs('./daaa',exist_ok=True) #exists_ok = true →如果該資料夾存在，不執行不會噴error
-
+os.makedirs('./data',exist_ok=True) #exists_ok = true →如果該資料夾存在，不執行不會噴error
+dirs = os.listdir('./data/')
 files = []
+for i in dirs:
+    files.append(i)
+
 if 'Homework.txt' in files:
     print('[O] , Homework.txt is exists!!')
 else:
     print('[X] , Homework.txt isn\'t exists!!')
 
-with open('./daaa/Homework.txt','w') as file:
+with open('./data/Homework.txt','w') as file:
     file.write('Hello World')
 
-with open('./daaa/Homework.txt','r') as file2:
+with open('./data/Homework.txt','r') as file2:
     data = file2.read()
 
 if len('Hello world') == len(data):
--- Day001/Day001_Sample.py
+++ Day001/Day001_Sample.py
@@ -1,4 +1,16 @@
 from urllib.request import urlretrieve
+import os , sys , chardet
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 urlretrieve ("https://www.w3.org/TR/PNG/iso_8859-1.txt", "./1.txt")
-urlretrieve ("https://www.w3.org/TR/PNG/iso_8859-1.txt", "./data/2.txt")
+urlretrieve ("https://www.w3.org/TR/PNG/iso_8859-1.txt", "./data/Homework.txt")
+dirs = os.listdir('./')
+for f in dirs:
+    print(f)
+
+a = open('./data/Homework.txt','r')
+f = a.read()
+print(f)
+print(chardet.detect(f))
